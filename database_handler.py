bba343169d49cce10ce0b83383eb987caaca97d7
Lucky <tharindalakshan@gmail.com>
2025-04-06 21:47:01 +0400

    06042025 with users database and add user function

--- database_handler.py
+++ database_handler.py
@@ -1,44 +1,43 @@
-def create_database_and_tables():  # sourcery skip: extract-method
+def connect_to_database():  # sourcery skip: extract-method
 
     import mysql.connector
 
-    workshop_inventory = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="admin"
+    return mysql.connector.connect(
+        host="localhost", user="root", password="admin"
     )
 
+def create_database_and_tables():    
+    workshop_inventory = connect_to_database()
+    
     if workshop_inventory.is_connected():
         print('Connected to the Database Server')
         dbCursor = workshop_inventory.cursor()
 
-        dbCursor.execute('SHOW DATABASES')
+        dbCursor.execute("SHOW DATABASES")
 
         if [database for  database in dbCursor if 'workshop_inventory' in database]:
             print('Database "workshop_inventory" already exists.')
         else:
-            dbCursor.execute('CREATE DATABASE workshop_inventory')
+            dbCursor.execute("CREATE DATABASE workshop_inventory")
             print('Database "workshop_inventory" has been created.')
 
         workshop_inventory.config(database='workshop_inventory')
         workshop_inventory.reconnect()
 
-        dbCursor.execute('SHOW TABLES')
+        dbCursor.execute("SHOW TABLES")
 
         if [table for table in dbCursor if 'users' in table]:
             print('Table "users" already exists.')
         else:
-            dbCursor.execute('CREATE TABLE users (user_id INT PRIMARY KEY,'
-                            'user_name VARCHAR(255) NOT NULL, is_active BOOLEAN NOT NULL)')
+            dbCursor.execute("CREATE TABLE users (emp_id INT PRIMARY KEY, user_name VARCHAR(255) NOT NULL, is_active BOOLEAN NOT NULL)")
             print('Table "users" has been created.')
 
-        dbCursor.execute('SHOW TABLES')
+        dbCursor.execute("SHOW TABLES")
 
         if [table for table in dbCursor if 'inventory' in table]:
             print('Table "inventory" already exists.')
         else:
-            dbCursor.execute('CREATE TABLE inventory (item_id INT PRIMARY KEY,'
-                            'item_name VARCHAR(255) NOT NULL, remaining_amount INT NOT NULL)')
+            dbCursor.execute("CREATE TABLE inventory (item_id INT PRIMARY KEY, item_name VARCHAR(255) NOT NULL, remaining_amount INT NOT NULL)")
             print('Table "inventory" has been created.')
 
         workshop_inventory.close()
@@ -46,5 +45,29 @@ def create_database_and_tables():  # sourcery skip: extract-method
     else:
         print('Connection Failed')
 
-if __name__ == '__main__':
-    create_database_and_tables()
+
+def read_from_database(table):
+    workshop_inventory = connect_to_database()
+    workshop_inventory.config(database='workshop_inventory')
+    workshop_inventory.reconnect()
+    dbCursor = workshop_inventory.cursor()
+
+    dbCursor.execute(f"SELECT * FROM {table}")
+    data = dbCursor.fetchall()
+    workshop_inventory.close()
+    print(data)
+
+
+def write_to_database(table, data):
+    workshop_inventory = connect_to_database()
+    workshop_inventory.config(database='workshop_inventory')
+    workshop_inventory.reconnect()
+    dbCursor = workshop_inventory.cursor()
+
+    empID = data[0]
+    userName = data[1]
+    isActive = data[2]
+
+    dbCursor.execute(f'INSERT INTO {table} (emp_id, user_name, is_active) VALUES ({empID}, "{userName}", {isActive})')
+    workshop_inventory.commit()
+    workshop_inventory.close()
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import database_handler
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, request, url_for, render_template
 
 
 database_handler.create_database_and_tables()
@@ -9,6 +9,19 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/add_items")
+def add_items():
+    return render_template("add_items.html")
+
+@app.route("/users", methods=["POST", "GET"])
+def manage_users():
+    database_handler.read_from_database("users")
+    if request.method == "POST":
+        empID = request.form["emp_id"]
+        username = request.form["user_name"]
+        database_handler.write_to_database("users", (empID, username, 1))
+    return render_template("/users.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
